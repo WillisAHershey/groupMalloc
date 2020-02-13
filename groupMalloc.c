72d6c3f23850d1c8c00b9cb6ba211207e2160961
Willis A. Hershey <48898590+WillisAHershey@users.noreply.github.com>
2020-02-13 14:46:14 -0500

    Files!

---
+++ groupMalloc.c
@@ -0,0 +1,126 @@
+#include "groupMalloc.h"
+
+int groupMallocInit(groupMalloc_t *group,size_t size){
+  if(!group||!size)
+	return 0;
+  size_t numPages,numObjs,numMaps;
+  if(size*MAPBITS+sizeof(groupMallocSlab_t)+sizeof(MAPTYPE)<=PAGESIZE){
+	for(numMaps=1;size*numMaps*MAPBITS+sizeof(MAPTYPE)*numMaps+sizeof(groupMallocSlab_t)<PAGESIZE;++numMaps)
+		;
+	if(PAGESIZE-sizeof(groupMallocSlab_t)-(numMaps+1)*sizeof(MAPTYPE)-size*MAPBITS*numMaps>size)
+		++numMaps;
+	numPages=1;
+	numObjs=(PAGESIZE-sizeof(groupMallocSlab_t)-numMaps*sizeof(MAPTYPE))/size;
+  }
+  else{
+	numPages=(size*MAPBITS)/PAGESIZE;
+	numPages=numPages;
+	if(PAGESIZE*numPages-2*sizeof(MAPTYPE)-sizeof(groupMallocSlab_t)-size*MAPBITS>size)
+		numMaps=2;
+	else
+		numMaps=1;
+	numObjs=(numPages*PAGESIZE-numMaps*sizeof(MAPTYPE)-sizeof(groupMallocSlab_t))/size;
+  }
+  groupMallocSlab_t *slab=MMAP_PAGESIZE(numPages);
+  if(slab==MAP_FAILED)
+	return 0;
+  group->start=group->current=slab;
+  group->objSize=size;
+  group->numPages=numPages;
+  group->numObjs=numObjs;
+  slab->next=NULL;
+  size_t c;
+  for(c=0;c<numObjs/MAPBITS;++c)
+	slab->maps[c]=~(MAPTYPE)0;
+  if(numObjs%MAPBITS)
+	slab->maps[c]=(((MAPTYPE)1)<<numObjs%MAPBITS)-1;
+#ifdef THREAD_SAFE
+  sem_init(&group->turn,0,1);
+#endif
+  return 1;
+}
+
+void* groupMalloc(groupMalloc_t* group){
+  groupMallocSlab_t *slab=group->current,*last;
+  size_t numMaps=group->numObjs%MAPBITS?group->numObjs/MAPBITS+1:group->numObjs/MAPBITS;
+  MAPTYPE mask;
+  size_t c,d;
+  wait(&group->turn);
+  for(c=0;c<numMaps;++c)
+	if(slab->maps[c])
+		for(({mask=(MAPTYPE)1;d=0;});d<MAPBITS;({mask<<=1;++d;}))
+			if(mask&slab->maps[c]){
+				slab->maps[c]&=~mask;
+				post(&group->turn);
+				return (void*)(((intptr_t)&slab->maps[numMaps])+group->objSize*(MAPBITS*c+d));
+			}
+  if(slab==group->start){
+	last=slab;
+	slab=NULL;
+  }
+  else
+	slab=group->start;
+  while(slab){
+	for(c=0;c<numMaps;++c)
+		if(slab->maps[c])
+			for(({mask=(MAPTYPE)1;d=0;});d<MAPBITS;({mask<<=1;++d;}))
+				if(mask&slab->maps[c]){
+					slab->maps[c]&=~mask;
+					post(&group->turn);
+					return (void*)(((intptr_t)&slab->maps[numMaps])+group->objSize*(MAPBITS*c+d));
+				}
+	last=slab;
+	slab=slab->next;
+  }
+  last->next=MMAP_PAGESIZE(group->numPages);
+  if(!last->next){
+	post(&group->turn);
+	return NULL;
+  }
+  slab=last->next;
+  slab->next=NULL;
+  d=group->numObjs/MAPBITS;
+  for(c=0;c<d;++c)
+	slab->maps[c]=~(MAPTYPE)0;
+  if(group->numObjs%MAPBITS)
+	slab->maps[c]=(((MAPTYPE)1)<<(group->numObjs%MAPBITS))-1;
+  --slab->maps[0];
+  group->current=slab;
+  post(&group->turn);
+  return (void*)&slab->maps[numMaps];
+}
+
+void groupFree(groupMalloc_t *group,void *pt){
+  groupMallocSlab_t *slab=group->start;
+  size_t numMaps=group->numObjs%MAPBITS?group->numObjs/MAPBITS+1:group->numObjs/MAPBITS;
+  MAPTYPE mask;
+  size_t c,d;
+  wait(&group->turn);
+  while(slab){
+	if((intptr_t)pt>(intptr_t)slab&&(intptr_t)pt<(intptr_t)slab+group->numPages*PAGESIZE)
+		for(c=0;c<numMaps;++c)
+			if(slab->maps[c]!=~(MAPTYPE)0)
+				for(({mask=(MAPTYPE)1;d=0;});d<MAPBITS;({mask<<=1;++d;}))
+					if((intptr_t)&slab->maps[numMaps]+group->objSize*(c*MAPBITS+d)==(intptr_t)pt){
+						if(slab->maps[c]&mask)
+							printf("Free invalid pointer\n");
+						slab->maps[c]|=mask;
+						post(&group->turn);
+						return;
+					}
+	slab=slab->next;
+  }
+  post(&group->turn);
+  return;
+}
+
+void groupMallocDestroy(groupMalloc_t *group){
+  groupMallocSlab_t *slab=group->start;
+  groupMallocSlab_t *next;
+  while(slab){
+	next=slab->next;
+	MUNMAP_PAGESIZE(slab,group->numPages);
+	slab=next;
+  }
+  return;
+}
---
+++ groupMalloc.h
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/mman.h>
+
+#ifdef THREAD_SAFE
+	#include <semaphore.h>
+	#define wait(c) sem_wait(c)
+	#define post(c) sem_post(c)
+#else
+	#define wait(c) {}
+	#define post(c) {}
+#endif
+
+#ifndef _GROUP_MALLOC
+#define _GROUP_MALLOC
+
+#ifndef MAP_UNINITIALIZED
+//This prevents errors in systems that don't use the MAP_UNINITIALIZED flag, which seems to be most of them
+	#define MAP_UNINITIALIZED 0
+#endif
+
+#define MAPTYPE unsigned long
+#define MAPBITS (sizeof(MAPTYPE)*8)
+#define PAGESIZE sysconf(_SC_PAGE_SIZE)
+#define MMAP_PAGESIZE(c) mmap(NULL,c*PAGESIZE,PROT_READ|PROT_WRITE,MAP_PRIVATE|MAP_ANONYMOUS|MAP_UNINITIALIZED,-1,0)
+#define MUNMAP_PAGESIZE(c,d) munmap(c,d*PAGESIZE)
+
+typedef struct groupMallocSlab{
+  struct groupMallocSlab *next;
+  MAPTYPE maps[0];
+}groupMallocSlab_t;
+
+typedef struct groupMalloc{
+  struct groupMallocSlab *start;
+  struct groupMallocSlab *current;
+  size_t objSize;
+  size_t numPages;
+  size_t numObjs;
+#ifdef THREAD_SAFE
+  sem_t turn;
+#endif
+}groupMalloc_t;
+
+int groupMallocInit(groupMalloc_t*,size_t); //Returns a pointer to a new groupMalloc structure configured for objects of a given size.
+void* groupMalloc(groupMalloc_t*); //Returns a pointer to an available space in memory that can fit the object
+void groupFree(groupMalloc_t*,void*); //Frees the space to the groupMalloc_t structure so that it can be repurposed on a future groupMalloc() call
+void groupMallocDestroy(groupMalloc_t*); //Frees all memory associated with the groupMalloc_t structure. Access to any data stored there will be lost.
+
+#endif
